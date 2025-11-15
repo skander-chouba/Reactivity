07bd7a526e2931ff1416db835f8c5ec354787302
Skander Chouba <skander.chouba@gmail.com>
2025-11-15 18:28:23 +0100

    Add project files.

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Activity> Activities { get; set; }
+
+    }
+}
